79ae9b573c686e58ef6ec1c707834c02490d6d15
sohye <sohye@squarelab.co>
2020-07-23 00:07:31 +0900

    env: dockerize (#3)

--- server/src/Server.ts
+++ server/src/Server.ts
@@ -2,19 +2,14 @@ import cookieParser from 'cookie-parser';
 import morgan from 'morgan';
 import path from 'path';
 import helmet from 'helmet';
-
 import express, { Request, Response, NextFunction } from 'express';
 import { BAD_REQUEST } from 'http-status-codes';
 import 'express-async-errors';
-
 import BaseRouter from './routes';
 import logger from '@shared/Logger';
 
-
 // Init express
 const app = express();
-
-
 
 /************************************************************************************
  *                              Set basic express settings
@@ -43,20 +38,6 @@ app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
     return res.status(BAD_REQUEST).json({
         error: err.message,
     });
-});
-
-
-
-/************************************************************************************
- *                              Serve front-end content
- ***********************************************************************************/
-
-const viewsDir = path.join(__dirname, 'views');
-app.set('views', viewsDir);
-const staticDir = path.join(__dirname, 'public');
-app.use(express.static(staticDir));
-app.get('*', (req: Request, res: Response) => {
-    res.sendFile('index.html', {root: viewsDir});
 });
 
 // Export express instance
--- server/src/index.ts
+++ server/src/index.ts
@@ -4,6 +4,10 @@ import logger from '@shared/Logger';
 
 // Start the server
 const port = Number(process.env.PORT || 3000);
+
+// TODO: Remove Debugging Code
+app.get('/', (req, res) => res.send('Hello World!'))
+
 app.listen(port, () => {
     logger.info('Express server started on port: ' + port);
 });
